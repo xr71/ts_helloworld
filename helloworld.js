17119085366c0d603bab128a3a19ee848314e80e
Xu Ren <musicalxu@hotmail.com>
2020-01-31 21:23:44 +0400

    init tsc

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,5 @@
 console.log("hello world");
+var input1 = document.getElementById("num1");
+function add(num1, num2) {
+    return num1 + num2;
+}
--- helloworld.ts
+++ helloworld.ts
@@ -1,2 +1,9 @@
 console.log("hello world");
 
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+
+    return num1 + num2;
+}
+
